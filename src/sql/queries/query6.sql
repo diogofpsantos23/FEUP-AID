a0787b98f75574db0a5071fba9553dd7c75e4021
manuelcralves <62514015+manuelcralves@users.noreply.github.com>
2026-01-02 13:47:51 +0000

    minor change query

--- src/sql/queries/query6.sql
+++ src/sql/queries/query6.sql
@@ -13,4 +13,4 @@ JOIN Dim_Location pu  ON pu.LocationKey = f.PickupLocationKey
 WHERE d.FullDate >= '2025-07-01' AND d.FullDate < '2025-08-01'
 GROUP BY f.PickupDateKey, d.FullDate, f.PickupLocationKey, pu.Zone
 ORDER BY Revenue DESC
-LIMIT 1;
+LIMIT 10;
